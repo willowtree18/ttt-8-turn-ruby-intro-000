2bc1b71ce964fc3919eb77a07b8e5f80e20f317c
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 16:34:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,3 +8,20 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
+
+def position_taken?(board, index)
+if board[index] == " " or board[index] == "" or board[index] == (nil)
+  return (false)
+
+else
+  return (true)
+end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return (true)
+
+  end
+
+end
